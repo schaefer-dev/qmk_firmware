4483d7cb9461a0e17fae191defc51e6177af501d
Daniel Schäfer <schaefer.dev@gmail.com>
2020-08-19 14:03:26 +0200

    vim style arrow keys on home-row

--- keyboards/hhkb/keymaps/schaeferdev/keymap.c
+++ keyboards/hhkb/keymaps/schaeferdev/keymap.c
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
       | `~   | F1  | F2  | F3  | F4 | F5 | F6 | F7 | F8  | F9  | F10 | F11 | F12   | DEL   | PWR |
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      | Caps |     | UP  |     |    |    |    |    | UP  | Slk | Pus | Up  |       | Backs |     |
+      | Caps |     | UP  |     |    |    |    |    |     | Slk | Pus | Up  |       | Backs |     |
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      |      |LEFT |DOWN |RIGHT |    |    | *  |LEFT|DOWN |RIGHT| Lef | Rig | Enter |       |     |
+      |      |LEFT |DOWN |RIGHT |    | * |Left|DOWN| UP |RIGHT|      |     | Enter |       |     |
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
       |      | VoD | VoU | Mut |    |    | +  | -  | End | PgD | Dow |     |       |       |     |
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
@@ -48,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [HHKB] = LAYOUT(
         KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_PWR,
-        KC_CAPS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UP, KC_SLCK, KC_PAUS, KC_UP, KC_TRNS, KC_BSPC,
-        KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_TRNS, KC_PAST, KC_LEFT, KC_DOWN, KC_RIGHT, KC_LEFT, KC_RGHT, KC_PENT,
+        KC_CAPS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SLCK, KC_PAUS, KC_UP, KC_TRNS, KC_BSPC,
+        KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS, KC_TRNS, KC_PENT,
         KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_PPLS, KC_PMNS, KC_END, KC_PGDN, KC_DOWN, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
